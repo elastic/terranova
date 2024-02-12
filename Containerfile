32bd2702fd436e02e005da34f9216ea08ef37621
Adrien Mannocci <adrien.mannocci@elastic.co>
2024-02-12 13:08:33 +0100

    ci: build for macos and linux (#1)
    * ci: build for macos and linux
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * feat: add release workflow
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * fix: forward the right arch platform
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * chore: use release step as part of the CI
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * fix: copy only binary
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * feat: implement release note automation
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * chore: global cleanup
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * refactor: common env setup steps
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * fix: remove otel workflow and add dependency'
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * chore: use slim base image
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * ci: re-introduce the opentelemetry workflow
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    * fix: install binutils for slim base
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>
    
    ---------
    
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>

---
+++ Containerfile
@@ -0,0 +1,25 @@
+# Base image for build
+ARG base_image_version=3.10.12
+FROM python:${base_image_version}-slim-bullseye as builder
+
+# Switch workdir
+WORKDIR /opt/terranova
+
+# Copy files
+COPY . .
+
+# Install build packages
+RUN \
+  apt-get update > /dev/null \
+  && apt-get install -y --no-install-recommends \
+    binutils="*" \
+  && apt-get clean
+
+# Install poetry
+RUN \
+  pip3 install --no-cache-dir --upgrade pip poetry
+
+# Build
+RUN \
+  poetry install \
+  && poetry run pyinstaller terranova.spec
---
+++ scripts/build.py
@@ -0,0 +1,80 @@
+import platform
+import sys
+from pathlib import Path
+from time import time
+
+from sh import git, pyinstaller
+
+from scripts.utils import Constants, container_backend, read_project_conf
+
+
+def run() -> None:
+    conf = read_project_conf()
+    commit_hash_short = git("rev-parse", "--short", "HEAD").strip()
+    current_time_epoch = int(time())
+    version = conf.get("tool.poetry.version")
+    python_version = platform.python_version()
+
+    image_id = f"{version}-{current_time_epoch}-{commit_hash_short}"
+
+    # Create dist dir
+    local_dist_path = Path("dist")
+    local_dist_path.mkdir(parents=True, exist_ok=False)
+    local_dist_path = local_dist_path.absolute()
+
+    system = platform.system().lower()
+    if system == "darwin":
+        pyinstaller("terranova.spec", _out=sys.stdout, _err=sys.stderr)
+        arch = platform.machine()
+        arch = "amd64" if arch == "x86_64" else arch
+        Path("./dist/terranova").replace(Path(f"./dist/terranova-{version}-{system}-{arch}"))
+    elif system == "linux":
+        # Use cross-build to build both amd64 and arm64 versions.
+        cmd, env = container_backend()
+        for arch in ["amd64", "arm64"]:
+            platform_arch = f"linux/{arch}"
+            cmd(
+                "buildx",
+                "build",
+                "--load",
+                "--platform",
+                platform_arch,
+                "--build-arg",
+                f"base_image_version={python_version}",
+                "-t",
+                f"{Constants.REGISTRY_URL}/terranova:{image_id}",
+                "-f",
+                "Containerfile",
+                ".",
+                _out=sys.stdout,
+                _err=sys.stderr,
+                _env=env,
+            )
+            container_id = cmd(
+                "run",
+                "-d",
+                "--platform",
+                platform_arch,
+                "--entrypoint=cat",
+                f"{Constants.REGISTRY_URL}/terranova:{image_id}",
+                _err=sys.stderr,
+                _env=env,
+            ).strip()
+            cmd(
+                "cp",
+                f"{container_id}:/opt/terranova/dist/terranova",
+                (local_dist_path / f"terranova-{version}-linux-{arch}").as_posix(),
+                _out=sys.stdout,
+                _err=sys.stderr,
+                _env=env,
+            )
+            cmd(
+                "rm",
+                "-f",
+                container_id,
+                _out=sys.stdout,
+                _err=sys.stderr,
+                _env=env,
+            )
+    else:
+        print(f"Unsupported system: {system}", file=sys.stderr)
