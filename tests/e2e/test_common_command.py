20fd459540f54a5229ed51ce5332960f2d543529
Adrien Mannocci <adrien.mannocci@elastic.co>
2024-09-03 19:12:10 +0100

    feat: rename entrypoint to cli (#90)
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>

--- tests/e2e/test_common_command.py
+++ tests/e2e/test_common_command.py
@@ -1,14 +1,14 @@
 from click.testing import CliRunner
 
-from terranova import terranova
-from terranova.terranova import main
+from terranova import cli
+from terranova.cli import main
 from tests.e2e.conftest import assert_result
 
 
 def test_version(runner: CliRunner) -> None:
     result = runner.invoke(main, args=["--version"])
     stdout, _ = assert_result(result)
-    assert f"terranova, version {terranova.__version__}" in stdout
+    assert f"terranova, version {cli.__version__}" in stdout
 
 
 def test_with_no_command(runner: CliRunner) -> None:
