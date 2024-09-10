2f39316180f79ae8c326b32ab506609b4e15ea17
Adrien Mannocci <adrien.mannocci@elastic.co>
2024-09-10 11:08:54 +0100

    feat: improve binary generation (#93)

--- scripts/generate.py
+++ scripts/generate.py
@@ -23,6 +23,18 @@ from scripts.utils import detect_pyinstaller
 
 def run() -> None:
     args = ["-n", "terranova", "--onefile", "--noconfirm"]
+    exclude_modules = (
+        "setuptools",
+        "setuptools._vendor",
+        "setuptools._vendor.importlib_metadata",
+    )
+    for exclude_module in exclude_modules:
+        args.extend(["--exclude-module", exclude_module])
+
+    hidden_imports = ()
+    for hidden_import in hidden_imports:
+        args.extend(["--hidden-import", hidden_import])
+
     args.extend(
         [
             "--add-data",
