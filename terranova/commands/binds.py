56639d12c08331185029c2a294fd45c49209152c
Victor Martinez <VictorMartinezRubio@gmail.com>
2024-10-14 13:51:39 +0200

    if None do nothing

--- terranova/commands/binds.py
+++ terranova/commands/binds.py
@@ -321,6 +321,7 @@ def plan(
         terraform = mount_context(full_path, import_vars=True)
 
         # Generate unique out path
+        unique_out_path = None
         if not out is None:
             unique_out_path = os.path.join(full_path, out)
 
