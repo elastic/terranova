5bd42e12350382298bb4b015c7eeaefe5a7ff659
Victor Martinez <VictorMartinezRubio@gmail.com>
2025-01-23 21:30:04 +0100

    feat: support install.sh script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+set -o errexit
+set -o nounset
+
+## NOTE: Bump this version when a new release.
+VERSION=0.6.4
+OS=$(uname -s| tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m| tr '[:upper:]' '[:lower:]')
+if [ "${ARCH}" = "aarch64" ] ; then
+    GVM_ARCH_SUFFIX=arm64
+elif [ "${ARCH}" = "x86_64" ] ; then
+    GVM_ARCH_SUFFIX=amd64
+elif [ "${ARCH}" = "arm64" ] ; then
+    GVM_ARCH_SUFFIX=arm64
+else
+    echo "Unsupported architecture: ${ARCH}"
+    exit 1
+fi
+
+URL="https://github.com/elastic/terranova/releases/download/${VERSION}/terranova-${VERSION}-${OS}-${GVM_ARCH_SUFFIX}"
+
+LOCATION="/usr/local/bin"
+curl -sSLo "$LOCATION/terranova" "$URL"
+chmod +x "$LOCATION/terranova"
