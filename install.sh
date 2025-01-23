bd44f6be5440d4e60e18c7f9b10ce95b8b4ff53b
Victor Martinez <VictorMartinezRubio@gmail.com>
2025-01-23 21:39:08 +0100

    rename

--- install.sh
+++ install.sh
@@ -8,17 +8,17 @@ VERSION="0.6.4"
 OS=$(uname -s| tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m| tr '[:upper:]' '[:lower:]')
 if [ "${ARCH}" = "aarch64" ] ; then
-    GVM_ARCH_SUFFIX=arm64
+    ARCH_SUFFIX=arm64
 elif [ "${ARCH}" = "x86_64" ] ; then
-    GVM_ARCH_SUFFIX=amd64
+    ARCH_SUFFIX=amd64
 elif [ "${ARCH}" = "arm64" ] ; then
-    GVM_ARCH_SUFFIX=arm64
+    ARCH_SUFFIX=arm64
 else
     echo "Unsupported architecture: ${ARCH}"
     exit 1
 fi
 
-URL="https://github.com/elastic/terranova/releases/download/${VERSION}/terranova-${VERSION}-${OS}-${GVM_ARCH_SUFFIX}"
+URL="https://github.com/elastic/terranova/releases/download/${VERSION}/terranova-${VERSION}-${OS}-${ARCH_SUFFIX}"
 
 LOCATION="/usr/local/bin"
 curl -sSLo "$LOCATION/terranova" "$URL"
